006293e73f83a91d31635d20653d43c4eef94007
Olofsson Robin <roblof-8@student.ltu.se>
2019-12-02 15:05:18 +0100

    nya V

--- Lab2ALGO/src/Main.java
+++ Lab2ALGO/src/Main.java
@@ -1,44 +1,56 @@
-
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 import java.util.ArrayList;
 
+/**
+ * @author roblof-8, johlax-8, wesjon-5
+ */
 public class Main {
 
+
     public static void main(String[] args) {
+        int[] ok = {0,1,1,3,4,2};
+        modifiedProbing(ok);
+        linearProbing(ok);
+    }
+
+    /**
+     * Modified probing
+     */
+    public static void modifiedProbing(int test[]){
         //variables
-        int[] ok = {35, 2, 1, 3, 75, 5, 6, 7,42,100};
-        int m = ok.length;
+        int m = test.length;
         int index;
         ArrayList<Triplet<Integer,Integer,Integer>> hasTable = new ArrayList<>();
 
         for(int i = 0; i < m; i++){
-            hasTable.add(new Triplet<Integer, Integer, Integer>(1,1,null));
+            hasTable.add(new Triplet<>(1,1,null));
         }
 
         //loops through the given list with Integers.
-        for (int x: ok
-             ) {
+        for (int x: test
+        ) {
             index = h(x,m);
             if(hasTable.get(index).getValue() != null){
 
-               // if(hasTable.get(index).getLup() < hasTable.get(index).getLdown()){
+                if(hasTable.get(index).getLup() <= hasTable.get(index).getLdown()){
                     //Calls f1 at the index + Lups index where we haven't checked yet.
                     hasTable = f1(x,hasTable,index);
 
-               // }
+                }else{
+                    hasTable = f2(x,hasTable,index);
+                }
             }else{
                 //If the hashtable position is empty, add x.
                 hasTable.get(index).setValue(x);
             }
         }
-
+        //print out table values
         for (int i = 0; i < hasTable.size(); i++){
 
-            System.out.println("Index: " + i + " Lup:" + hasTable.get(i).getLup() + " och value:" + hasTable.get(i).getValue());
+            System.out.println("Index: " + i + " Lup:" + hasTable.get(i).getLup() + " Ldown: "+ hasTable.get(i).getLdown() + " och value:" + hasTable.get(i).getValue());
         }
-
     }
+
+
 
     public static ArrayList<Triplet<Integer,Integer,Integer>> f1(int x, ArrayList<Triplet<Integer,Integer,Integer>> arr, int index){
         //Variables
@@ -62,13 +74,89 @@ public class Main {
             }
             j++;
             tempIndex++;
+        }
+    }
 
+    public static ArrayList<Triplet<Integer,Integer,Integer>> f2(int x, ArrayList<Triplet<Integer,Integer,Integer>> arr, int index){
+        //Variables
+        int m = arr.size();
+        int j = 1;
+        int tempIndex = index - arr.get(index).getLdown();
+        int add;
+
+        while(true){
+            //in case tempIndex is out of range.
+            if(tempIndex < 0){
+                tempIndex = m + tempIndex;
+            }
+
+            //if the tempIndex is empty, add the element to the list and save the new lup value.
+            if(arr.get(tempIndex).getValue() == null){
+                arr.get(tempIndex).setValue(x);
+                add = arr.get(index).getLdown();
+                arr.get(index).setLDown(add+j);
+                return arr;
+            }
+            j++;
+            tempIndex--;
+        }
+    }
+    /**
+     *
+     * @param x Integer in the list
+     * @param m Length of the list
+     * @return index in the hashtable list.
+     */
+    public static int h(int x, int m) {
+        return x % m;
+    }
+
+    public static void linearProbing(int[] test){
+        //Variables
+        ArrayList<Integer> hashtable = new ArrayList<>();
+        int index;
+        int m = test.length;
+
+        //instances the list with null objects
+        for (int i = 0; i < m; i++){
+            hashtable.add(null);
+        }
+
+
+        for (int x:test
+             ) {
+            //calls h(x) for index value
+            index = h(x,m);
+            //if the spot is taken run linearf1()
+            if(hashtable.get(index) != null){
+                hashtable = linearF1(x,hashtable,index);
+            }else{
+                hashtable.set(index,x);
+            }
+        }
+        for(int i = 0; i < hashtable.size(); i++){
+            System.out.println("Index:" + i + " Value: " + hashtable.get(i));
         }
 
 
     }
 
-    public static int h(int x, int m) {
-        return x % m;
+    public static ArrayList<Integer> linearF1(int x, ArrayList<Integer> arr, int index){
+        int tempIndex = index + 1;
+        int m = arr.size();
+
+        while(true){
+
+            //range out of bound.
+            if(tempIndex == m){
+                tempIndex = 0;
+            }
+            if(arr.get(tempIndex) == null){
+                arr.set(tempIndex,x);
+                return arr;
+            }
+            tempIndex++;
+        }
+
     }
 }
--- Lab2ALGO/src/Triplet.java
+++ Lab2ALGO/src/Triplet.java
@@ -1,35 +1,78 @@
+/**
+ *
+ * @param <L> Left generic
+ * @param <M> Middle generic
+ * @param <R> Right generic
+ *
+ * Triplet class, represent an object that can hold 3 different generics.
+ *
+ * @author roblof-8, johlax-8 and vesjon-5
+ *
+ *
+ */
+
 public class Triplet<L,M,R> {
     private L l;
     private M m;
     private R r;
 
+    /**
+     *
+     * @param l left generic
+     * @param m right generic
+     * @param r middle generic
+     */
     public Triplet(L l,M m, R r){
         this.l =l;
         this. m = m;
         this.r = r;
     }
+
+    /**
+     *
+     * @return the value of LUp
+     */
     public L getLup(){
         return l;
     }
 
+    /**
+     *
+     * @return the value of LDown
+     */
     public M getLdown() {
         return m;
     }
 
+    /**
+     *
+     * @return the value at the given index
+     */
     public R getValue() {
         return r;
     }
 
+    /**
+     *
+     * @param l value given to left generic
+     */
     public void setLup(L l) {
         this.l = l;
     }
 
+    /**
+     *
+     * @param m value given to middle generic
+     */
     public void setLDown(M m) {
         this.m = m;
     }
 
+    /**
+     *
+     * @param r value given to right generic
+     */
     public void setValue(R r) {
         this.r = r;
     }
-
 }
